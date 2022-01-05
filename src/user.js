b1d3563f409cb2b9ff7c8bc3127627a88fc0564b
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-05 11:08:41 +0530

    user.

---
+++ src/user.js
@@ -0,0 +1,39 @@
+const mysql = require("mysql");
+const Promise = require("bluebird");
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+
+const dbinfo = {
+  host: "localhost",
+  user: "root",
+  password: "mysql",
+  database: "project1",
+};
+
+const selectAllUser = async () => {
+  const connection = mysql.createConnection(dbinfo);
+
+  await connection.connectAsync();
+
+  let sql = `SELECT * FROM user`;
+  // let sql = `SELECT * FROM user ORDER BY ID DESC`;
+  // let sql = `SELECT * FROM user WHERE ID=?`;
+  const list = await connection.queryAsync(sql, [10]);
+
+  console.log(list);
+
+  await connection.endAsync();
+};
+
+const addUser = async (user) => {
+  const connection = mysql.createConnection(dbinfo);
+
+  await connection.connectAsync();
+
+  let sql = `INSERT INTO user (username, password) values (?, ?)`;
+  connection.queryAsync(sql, [user.username, user.password]);
+  console.log("Record Added!");
+
+  await connection.endAsync();
+};
+
+module.exports = { selectAllUser, addUser };
