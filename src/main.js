7f96dfce8a2d327b9de66cf6a1b6ce79ced850f1
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-06 09:28:10 +0530

    basic. postbasic.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.get("/users", (req, res) => {
+  console.log(req.query);
   res.json({ message: "I AM TRYINTN TO  GET ALL USERS" });
 });
 
 app.post("/add-user", (req, res) => {
-  res.json({ message: "I AM TRYING TO Add New User" });
+  console.log(req.body);
+  res.json({ message: "I AM TRYING TO Add New User Kuch Bhiiii" });
 });
 
 app.listen(4000, () => console.log("server started"));
