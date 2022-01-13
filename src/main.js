ebecc8f9d771dbb1bfb84c080505b1a91b8aa073
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-13 11:15:18 +0530

    b7

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const app = express();
-// THIS IS HELPING TO READ THE :: "POST REQUEST BODY".
 app.use(express.json());
-
-// CROSS ORIGIN REQUEST ENABLING.
-const cors = require("cors");
-app.use(cors());
 
 const { addUser, selectAllUser } = require("./user");
 
-app.get("/users", async (req, res) => {
-  const list = await selectAllUser();
+// The "/messages" can vary student can type anything
+// The purpose is get all the messages
+app.get("/messages", async (req, res) => {
+  const list = await selectAllMessages();
   res.json(list);
 });
 
-app.post("/add-user", async (req, res) => {
+// The "/messages" can vary student can type anything
+// The purpose is add new message
+app.post("/add-message", async (req, res) => {
   const user = req.body;
   await addUser(user);
-  res.json({ message: "User Added Successfully" });
+  res.json({ message: "Message Added Successfully" });
 });
 
+// The PORT NUMBER Also can vary.
 app.listen(4000, () => console.log("this is optional, server started"));
